9d0a4a3a949efd4120dbf6dbb4291fba010b036a
Josiah <koreanhalo@gmail.com>
2016-12-01 14:08:02 -0600

    hopefully handle inputs from front end

--- news/views.py
+++ news/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from modules.articlefinder import *
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    elif request.method == 'POST':
+        article = ArticleFinder(request.data["article"], request.data["question"])
+        return render(request, 'index.html', {"url": article.finalURL})
 
 
-def db(request):
+#def db(request):
 
-    greeting = Greeting()
-    greeting.save()
+#    greeting = Greeting()
+#    greeting.save()
 
-    greetings = Greeting.objects.all()
+#    greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+#    return render(request, 'db.html', {'greetings': greetings})
 
